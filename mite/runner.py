be3b1e592a371907df42b58d8224865197b6cfdd
Aaron Ecay <aaronecay@gmail.com>
2019-07-03 11:07:01 +0100

    context.py: updates
    - use asynccontextmanager instead of custom classes
    - make sending functions async
    - compress exception handler and start_transaction into one
    - rename and tidy some things

--- mite/runner.py
+++ mite/runner.py
@@ -148,11 +148,10 @@ class Runner:
     async def _execute(self, context, scenario_id, scenario_data_id, journey_spec, args):
         logger.debug('Runner._execute starting scenario_id=%r scenario_data_id=%r journey_spec=%r args=%r',
                      scenario_id, scenario_data_id, journey_spec, args)
-        async with context._exception_handler():
-            async with context.transaction('__root__'):
-                journey = spec_import(journey_spec)
-                if args is None:
-                    await journey(context)
-                else:
-                    await journey(context, *args)
+        async with context.transaction('__root__'):
+            journey = spec_import(journey_spec)
+            if args is None:
+                await journey(context)
+            else:
+                await journey(context, *args)
         return scenario_id, scenario_data_id
