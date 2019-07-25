74e5d6bffdc4e4669baaa337a55246027888587d
Aaron Ecay <aaron.ecay@sky.uk>
2019-07-25 17:18:54 +0100

    add perftest suite (#26)

---
+++ test/perf/run-perftest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd `git rev-parse --show-toplevel`
+
+# if ! git diff-index --quiet HEAD -- ; then
+#     echo "Repo is dirty!!!"
+#     exit 1
+# fi
+
+docker build -t mite-perftest -f Dockerfile.perftest . || exit 1
+
+docker run --user `id -u` --rm \
+       --mount "type=bind,source=`pwd`/test/perf/output,destination=/output" \
+       --env MITE_PERFTEST_OUT=/output \
+       mite-perftest python test/perf/perftest.py \
+       `git rev-parse --abbrev-ref HEAD`-`git rev-parse --short HEAD`
