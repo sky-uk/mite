33bc115b367546c314c113dbdf20bd1151349916
Aaron Ecay <aaronecay@gmail.com>
2019-08-15 14:26:40 +0100

    no venv, try pip3 instead

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-python3 -m venv mite-tests
-
-source mite-tests/bin/activate
-
-pip install -r requirements.txt || exit 1
-pip install -r dev-requirements.txt || exit 1
+pip3 install -r requirements.txt || exit 1
+pip3 install -r dev-requirements.txt || exit 1
 
 tox; TOX_EXIT_CODE=$?
 coverage html
