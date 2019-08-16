738c55de3cab8ad27840afb891bbc9d4926d86bc
Aaron Ecay <aaronecay@gmail.com>
2019-08-16 16:30:44 +0100

    datapools: add one unit test

--- test/test_datapools.py
+++ test/test_datapools.py
@@ -1,5 +1,5 @@
 from mite.datapools import create_iterable_data_pool_with_recycling, create_iterable_data_pool, DataPoolExhausted
-
+import mite.datapools as dps
 
 def test_recycling():
     iterable = 'abcdefgh'
@@ -24,3 +24,19 @@ def test_iterable():
         pass
     else:
         assert False, "Data pool should have been exhausted"
+
+
+def test_recyclable_is_exhausted():
+    iterable = "ab"
+    dp = dps.RecyclableIterableDataPool(iterable)
+    xa = dp.checkout()
+    assert xa.data == "a"
+    x = dp.checkout()
+    assert x.data == "b"
+    x = dp.checkout()
+    assert x is None
+    dp.checkin(xa.id)
+    x = dp.checkout()
+    assert x.data == "a"
+    x = dp.checkout()
+    assert x is None
