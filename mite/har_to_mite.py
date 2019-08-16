9599ad3a085c7e918dba2278c7538f854200879d
Jordan Brennan <jb098@users.noreply.github.com>
2019-08-16 17:04:28 +0100

    Issue50 har unit tests (#63)
    * Added 2 new unit tests for har to mite
    
    * Skipped coverage on simple function, created render function but test unfinished

--- mite/har_to_mite.py
+++ mite/har_to_mite.py
@@ -42,15 +42,52 @@ def set_request_body(method, page):
     return ""
 
 
+def _parse_urls(pages):
+    """Parses urls from pages in hard file"""
+    return [page['title'] for page in pages['log']['pages']]    
+
+
+def _extract_and_sort_requests(pages):
+    """Pull entries from har text and sort into chronological order"""
+    entries = pages['log']['entries']
+    entries.sort(key=lambda n: n["startedDateTime"])
+    return entries
+
+def _create_journey_file_start(): # pragma: no cover
+    """The lines needed for imports and journey signature needed for
+    a working mite script. pragma ensures this is not counted for code
+    coverage"""
+    journey_start = "from .utils import check_status_code, check_status_code_in_groups\n"
+    journey_start += "from mite_browser import browser_decorator\n"
+    journey_start += "from mite.exceptions import MiteError\n"
+    journey_start += "from asyncio import sleep\n\n\n"
+    journey_start += "@browser_decorator()\n"
+    journey_start += "async def journey(ctx):\n"
+    return journey_start
+
+
+def _render_journey_transaction(page, req_method, expected_status_code, group_status, sleep_s):
+    """Renders a single transaction with a predefined template for use
+    in a mite journey"""
+    return TEMPLATE.render(
+        date_time=page['startedDateTime'],
+        method=req_method,
+        url=cur_page['request']['url'],
+        headers=set_request_headers_dict(cur_page),
+        json=set_request_body(req_method, cur_page),
+        check_groups=group_status,
+        expected_status=expected_status_code,
+        sleep=sleep_s)
+
+
 def har_convert_to_mite(file_name, converted_file_name, sleep_s):
     # TODO: accurate sleep times should be made possible by extracting the timestamps from the har file
     base_path = os.getcwd()
     with open(base_path + '/' + file_name.lstrip('/'), 'r') as f:
         temp_pages = json.loads(f.read())
     journey_main = ""
-    page_urls = [page['title'] for page in temp_pages['log']['pages']] 
-    entries = temp_pages['log']['entries']
-    entries.sort(key=lambda n: n["startedDateTime"])
+    page_urls = _parse_urls(temp_pages)
+    entries = _extract_and_sort_requests(temp_pages)
 
     for cur_page in entries:        
         if not cur_page['response']['status'] or not cur_page['request']['url'] in page_urls:
@@ -60,25 +97,13 @@ def har_convert_to_mite(file_name, converted_file_name, sleep_s):
         req_method = cur_page['request']['method'].lower()
 
         # main part of the journey
-        journey_main += TEMPLATE.render(
-            date_time=cur_page['startedDateTime'],
-            method=req_method,
-            url=cur_page['request']['url'],
-            headers=set_request_headers_dict(cur_page),
-            json=set_request_body(req_method, cur_page),
-            check_groups=check_groups_status,
-            expected_status=expected_status_code,
-            sleep=sleep_s)
-
-
-    # first part of the journey
-    journey_start = "from .utils import check_status_code, check_status_code_in_groups\n"
-    journey_start += "from mite_browser import browser_decorator\n"
-    journey_start += "from mite.exceptions import MiteError\n"
-    journey_start += "from asyncio import sleep\n\n\n"
-    journey_start += "@browser_decorator()\n"
-    journey_start += "async def journey(ctx):\n"
-    #journey_start += "    # import ipdb; ipdb.set_trace()\n\n"
+        journey_main += _render_journey_transaction(cur_page,
+                                                    req_method,
+                                                    expected_status_code,
+                                                    check_groups_status,
+                                                    sleep_s)
+       
+    journey_start = _create_journey_file_start_()
 
     with open(base_path + '/' + converted_file_name.lstrip('/'), 'w') as nf:
         nf.write(journey_start + journey_main)
