9e16a9011e0b5a627f3895587242def76abdb7a5
Aaron Ecay <aaronecay@gmail.com>
2019-08-19 14:33:52 +0100

    getting closer

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3.7 -m venv mite-tests
-source mite-tests/bin/activate
+. mite-tests/bin/activate
 
 pip install -r requirements.txt || exit 1
 pip install -r dev-requirements.txt || exit 1
