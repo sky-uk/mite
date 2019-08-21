529565154c60d6099555b6a6a8bb00b0bf8a2649
Aaron Ecay <aaronecay@gmail.com>
2019-08-21 10:53:04 +0100

    More shitty pipeline fixes
    Because the core platform is so shit and we canʼt test changes locally,
    only push them and see if they work, like a million monkeys taking a
    dump on a million typewriters

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-python3.7 -m venv /mite-tests
-. /mite-tests/bin/activate
+MY_VENV=$HOME/mite-tests
+
+python3.7 -m venv $MY_VENV
+. $MY_VENV/bin/activate
 
 pip install -r requirements.txt || exit 1
 pip install -r dev-requirements.txt || exit 1
