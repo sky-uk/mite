62e29f93e895391f6e68dc67799bbad65cc94815
Aaron Ecay <aaronecay@gmail.com>
2019-09-04 13:20:52 +0100

    fix up comments

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -17,9 +17,14 @@ coverage html
 
 flake8; FLAKE8_EXIT_CODE=$?
 
-# Further ideas for jobs to run: license check, black (once we're sure that
-# the pipeline is working): integration/performance tests, documentation
-# coverage, docs build (on master only), force test coverage to monotonically
-# increase, ...
+# Further ideas for jobs to run:
+# - license check
+# - black
+# - make sure test coverage increases
+# And, once we're sure that the pipeline is working:
+# - integration/performance tests
+# Once we have docs:
+# - documentation coverage
+# - docs build (on master only)
 
 [ $TOX_EXIT_CODE -eq 0 -a $FLAKE8_EXIT_CODE -eq 0 ] || exit 1
