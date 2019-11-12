76a24348e311fa6d75b86b4020813d184726b01e
Aaron Ecay <aaronecay@gmail.com>
2019-11-12 14:17:39 +0000

    Finish amqp wrapper first pass

--- mite_amqp/__init__.py
+++ mite_amqp/__init__.py
@@ -1,37 +1,44 @@
-import logging
 import asyncio
-import aio-pika
-
+import logging
 from contextlib import asynccontextmanager
-from mite.utils import spec_import
+
+import aio_pika
 
 logger = logging.getLogger(__name__)
 
 
 class _AMQPWrapper:
-    def __init__(self, context):
+    def __init__(self):
         self._loop = asyncio.get_event_loop()
+
+    def install(self, context):
         context.amqp = self
 
+    def uninstall(self, context):
+        del context.amqp
+
     async def connect_robust(self, *args, **kwargs):
-        if 'loop' not in kwargs:
-            kwargs['loop'] = self._loop
+        kwargs.setdefault("loop", self._loop)
         return await aio_pika.connect_robust(*args, **kwargs)
 
-    async def connect(self):
-        if 'loop' not in kwargs:
-            kwargs['loop'] = self._loop
+    async def connect(self, *args, **kwargs):
+        kwargs.setdefault("loop", self._loop)
         return await aio_pika.connect(*args, **kwargs)
 
 
 @asynccontextmanager
 async def _amqp_context_manager(context):
-    aw = _AMQPWrapper(context)
-    yield
+    aw = _AMQPWrapper()
+    aw.install(context)
+    try:
+        yield
+    finally:
+        aw.uninstall(context)
 
 
 def mite_amqp(func):
     async def wrapper(ctx, *args, **kwargs):
         async with _amqp_context_manager(ctx):
             return await func(ctx, *args, **kwargs)
+
     return wrapper
---
+++ test/test_mite_amqp.py
@@ -0,0 +1,54 @@
+import asyncio
+from unittest.mock import patch
+
+import pytest
+from asyncmock import AsyncMock
+from mite_amqp import mite_amqp
+from mocks.mock_context import MockContext
+
+
+@pytest.mark.asyncio
+async def test_mite_amqp_decorator():
+    context = MockContext()
+
+    @mite_amqp
+    async def dummy_journey(ctx):
+        assert ctx.amqp is not None
+
+    await dummy_journey(context)
+
+
+@pytest.mark.asyncio
+# FIXME new for 3.8
+# @unittest.mock.patch("aio_pika.connect")
+async def test_mite_amqp_connect():
+    context = MockContext()
+    url = "amqp://foo.bar"
+
+    connect_mock = AsyncMock()
+
+    @mite_amqp
+    async def dummy_journey(ctx):
+        await ctx.amqp.connect(url)
+
+    with patch("aio_pika.connect", new=connect_mock):
+        await dummy_journey(context)
+
+    connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
+
+
+@pytest.mark.asyncio
+async def test_mite_amqp_connect_robust():
+    context = MockContext()
+    url = "amqp://foo.bar"
+
+    connect_mock = AsyncMock()
+
+    @mite_amqp
+    async def dummy_journey(ctx):
+        await ctx.amqp.connect_robust(url)
+
+    with patch("aio_pika.connect_robust", new=connect_mock):
+        await dummy_journey(context)
+
+    connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
