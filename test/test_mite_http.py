425b19391b625ca4aee239292c95327e4cf3f5b0
Aaron Ecay <aaronecay@gmail.com>
2020-01-10 11:23:57 +0000

    fix test case

--- test/test_mite_http.py
+++ test/test_mite_http.py
@@ -24,7 +24,7 @@ async def test_additional_metrics(httpserver):
 
     @mite_http
     async def test(ctx):
-        ctx.additional_http_metrics = {"test_metric_name": 1}
+        ctx.http.additional_metrics = {"test_metric_name": 1}
         await ctx.http.get(httpserver.url_for("/test"))
 
     await test(context)
