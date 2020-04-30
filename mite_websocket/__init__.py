c61358f9bd2449de2c356a4e5f14d00d31f9be32
Jordan Brennan <jb098@users.noreply.github.com>
2020-04-30 15:37:41 +0100

    First commit for a mite websocket adaptor (#126)
    * First commit for a mite websocket adaptor
    
    * stylistic fixes
    
    * Add connection close on uninstall
    
    * bump version of the coverage library
    
    * CI fixes
    
    Co-authored-by: lsd runner <runner@lsd-sky.com>
    Co-authored-by: Aaron Ecay <aaronecay@gmail.com>

---
+++ mite_websocket/__init__.py
@@ -0,0 +1,53 @@
+import logging
+from contextlib import asynccontextmanager
+
+import websockets
+from websockets.exceptions import WebSocketException
+
+from mite.exceptions import MiteError
+
+logger = logging.getLogger(__name__)
+
+
+class WebsocketError(MiteError):
+    pass
+
+
+class _WebsocketWrapper:
+    def __init__(self):
+        self.connection = None
+
+    def install(self, context):
+        context.websocket = self
+
+    async def uninstall(self, context):
+        if self.connection:
+            await self.connection.close()
+        del context.websocket
+
+    async def connect(self, *args, **kwargs):
+        self.connection = await websockets.connect(*args, **kwargs)
+        return self.connection
+
+    async def send(self, body, **kwargs):
+        await self.connection.send(body)
+
+
+@asynccontextmanager
+async def _websocket_context_manager(context):
+    ww = _WebsocketWrapper()
+    ww.install(context)
+    try:
+        yield
+    except WebSocketException as e:
+        raise WebsocketError(e) from e
+    finally:
+        await ww.uninstall(context)
+
+
+def mite_websocket(func):
+    async def wrapper(ctx, *args, **kwargs):
+        async with _websocket_context_manager(ctx):
+            return await func(ctx, *args, **kwargs)
+
+    return wrapper
---
+++ test/test_mite_websockets.py
@@ -0,0 +1,65 @@
+from unittest.mock import patch
+
+import pytest
+from asyncmock import AsyncMock
+from mocks.mock_context import MockContext
+from websockets.exceptions import WebSocketException
+
+from mite_websocket import WebsocketError, mite_websocket
+
+
+@pytest.mark.asyncio
+async def test_mite_websocket_decorator():
+    context = MockContext()
+
+    @mite_websocket
+    async def dummy_journey(ctx):
+        assert ctx.websocket is not None
+
+    await dummy_journey(context)
+
+
+@pytest.mark.asyncio
+async def test_mite_websocket_decorator_uninstall():
+    context = MockContext()
+    connect_mock = AsyncMock()
+
+    @mite_websocket
+    async def dummy_journey(ctx):
+        await ctx.websocket.connect("wss://foo.bar")
+
+    with patch("websockets.connect", new=connect_mock):
+        await dummy_journey(context)
+
+    assert getattr(context, "websocket", None) is None
+
+
+@pytest.mark.asyncio
+async def test_mite_websocket_connect_and_send():
+    context = MockContext()
+    url = "wss://foo.bar"
+    message = "baz"
+    connect_mock = AsyncMock()
+
+    @mite_websocket
+    async def dummy_journey(ctx):
+        await ctx.websocket.connect(url)
+        await ctx.websocket.send(message)
+
+    with patch("websockets.connect", new=connect_mock):
+        await dummy_journey(context)
+
+    connect_mock.assert_called_once_with(url)
+    connect_mock.return_value.send.assert_called_once_with(message)
+
+
+@pytest.mark.asyncio
+async def test_mite_websocket_exception_handling():
+    context = MockContext()
+
+    @mite_websocket
+    async def dummy_journey(ctx):
+        raise WebSocketException("Something went wrong")
+
+    with pytest.raises(WebsocketError):
+        await dummy_journey(context)
