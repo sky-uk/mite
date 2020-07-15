05019b56fd879d921d63931409a3aa680a7b1f70
lsd runner <runner@lsd-sky.com>
2020-07-15 12:15:36 +0000

    plugins

--- mite_dns/__init__.py
+++ mite_dns/__init__.py
@@ -8,7 +8,7 @@ def _generate_stats():
     bins = [
         float(x)
         for x in os.environ.get(
-            "MITE_HTTP_HISTOGRAM_BUCKETS",
+            "MITE_DNS_HISTOGRAM_BUCKETS",
             "0.0001,0.001,0.01,0.05,0.1,0.2,0.4,0.8,1,2,4,8,16,32,64",
         ).split(",")
     ]
