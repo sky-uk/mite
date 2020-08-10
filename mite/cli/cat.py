dd0fd23855c57fbe4d9f22ad4a31b0a125f7363f
Aaron Ecay <aaronecay@gmail.com>
2020-08-10 17:06:17 +0100

    more cat fixes

--- mite/cli/cat.py
+++ mite/cli/cat.py
@@ -8,12 +8,13 @@ from mite.utils import pack_msg
 
 def cat(opts):
     with open(opts['MSGPACK_FILE_PATH'], 'rb') as file_in:
-        unpacker = msgpack.Unpacker(file_in, use_list=False)
+        unpacker = msgpack.Unpacker(file_in, use_list=False, raw=False)
         for row in unpacker:
-            print(row)
+            json.dump(row, sys.stdout)
+            sys.stdout.write("\n")
 
 
 def uncat(opts):
     for line in sys.stdin.readlines():
         value = json.loads(line)
-        sys.stdout.write(pack_msg(value))
+        sys.stdout.buffer.write(pack_msg(value))
