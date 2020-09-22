d74db772fdcdb861b3c2ce7e911034b591249f54
Aaron Ecay <aaronecay@gmail.com>
2020-09-22 15:45:08 +0100

    walrusectomy

--- mite/cli/collector.py
+++ mite/cli/collector.py
@@ -14,7 +14,8 @@ def _collector_receiver(opts):
 def collector(opts):
     receiver = _collector_receiver(opts)
     filter_fn = None
-    if filter_fn_spec := opts["--collector-filter"]:
+    filter_fn_spec = opts["--collector-filter"]
+    if filter_fn_spec:
         filter_fn = spec_import(filter_fn_spec)
     collector = Collector(
         target_dir=opts["--collector-dir"],
