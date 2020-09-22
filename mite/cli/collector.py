0d6361d6674ae9fe48a1773ec21d6e13bcc9cf99
Aaron Ecay <aaronecay@gmail.com>
2020-09-22 13:01:05 +0100

    Add filter function option to the collector

---
+++ mite/cli/collector.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from ..collector import Collector
+from ..utils import _msg_backend_module, spec_import
+
+
+def _collector_receiver(opts):
+    socket = opts["--collector-socket"]
+    receiver = _msg_backend_module(opts).Receiver()
+    receiver.connect(socket)
+    return receiver
+
+
+def collector(opts):
+    receiver = _collector_receiver(opts)
+    filter_fn = None
+    if filter_fn_spec := opts["--collector-filter"]:
+        filter_fn = spec_import(filter_fn_spec)
+    collector = Collector(
+        target_dir=opts["--collector-dir"],
+        roll_after=int(opts["--collector-roll"]),
+        filter_fn=filter_fn,
+    )
+    receiver.add_raw_listener(collector.process_raw_message)
+    asyncio.get_event_loop().run_until_complete(receiver.run())
--- mite/collector.py
+++ mite/collector.py
@@ -3,41 +3,56 @@ import os
 import time
 from itertools import count
 
+from .utils import unpack_msg
+
 logger = logging.getLogger(__name__)
 
 
 class Collector:
-    def __init__(self, target_dir=None, roll_after_n_messages=100000, collector_id=None):
-        logger.info('Starting collector')
+    def __init__(
+        self, target_dir=None, roll_after=100000, collector_id=None, filter_fn=None,
+    ):
+        logger.info('Initializing collector')
         if target_dir is None:
             target_dir = 'collector_data'
         self._target_dir = os.path.abspath(target_dir)
-        self._roll_after_n_messages = roll_after_n_messages
-        try:
-            os.makedirs(self._target_dir)
-        except FileExistsError:
-            pass
-        self._file_counter = count()
-        self._current_fn = os.path.join(self._target_dir, 'current')
-        self._current_st_fn = os.path.join(self._target_dir, 'current_start_time')
+        self._roll_after = roll_after
+        os.makedirs(self._target_dir, exist_ok=True)
         self._collector_id = collector_id
+        self._filter_fn = filter_fn
 
+        self._current = None
+        self._file_counter = count()
         self._rotate_current_file()
-        self._current = open(self._current_fn, 'wb')
-        self._msg_count = 0
-        self._tps_start = time.time()
-        self._tps_count = 0
+
+    @property
+    def _current_fn(self):
+        return os.path.join(self._target_dir, 'current')
+
+    @property
+    def _current_st_fn(self):
+        return os.path.join(self._target_dir, 'current_start_time')
 
     def process_raw_message(self, raw):
         self._msg_count += 1
-        self._current.write(raw)
-        if self._msg_count == self._roll_after_n_messages:
-            self._msg_count = 0
-            self._current.close()
+        if self._filter_fn:
+            decoded = unpack_msg(raw)
+            if self._filter_fn(decoded):
+                self._write_msg(raw)
+        else:
+            self._write_msg(raw)
+        if self._msg_count == self._roll_after:
             self._rotate_current_file()
-            self._current = open(self._current_fn, 'wb')
+
+    def _write_msg(self, msg):
+        self._current.write(msg)
 
     def _rotate_current_file(self):
+        self._msg_count = 0
+
+        if self._current:
+            self._current.close()
+
         if os.path.isfile(self._current_fn):
             logger.debug('rotating existing current file %s', self._current_fn)
             with open(self._current_st_fn) as f:
@@ -61,3 +76,5 @@ class Collector:
 
         with open(self._current_st_fn, 'w') as f:
             f.write(str(int(time.time())))
+
+        self._current = open(self._current_fn, 'wb')
