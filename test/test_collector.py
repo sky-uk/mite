ec89e239566135c2d9f765eab7257fa28644133a
Aaron Ecay <aaron.ecay@sky.uk>
2020-09-23 12:59:51 +0100

    Fix error in #152 relating to the use-json cli arg (#154)
    also, overhaul the test suite

--- test/test_collector.py
+++ test/test_collector.py
@@ -1,15 +1,18 @@
-import glob
 import json
 import os
 import tempfile
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
+from docopt import docopt
+
+import mite.__main__ as main
+import mite.cli.collector as cli_collector
 from mite.collector import Collector
 from mite.utils import pack_msg
 
 raw_msg = "  TEST_MSG_"
-check_msg_value = "TEST_MSG_0  TEST_MSG_1  TEST_MSG_2  TEST_MSG_3  TEST_MSG_4"
-check_msg_rollback = " TEST_MSG_5"
+check_msg_value = b"TEST_MSG_0  TEST_MSG_1  TEST_MSG_2  TEST_MSG_3  TEST_MSG_4"
+check_msg_rollback = b" TEST_MSG_5"
 
 
 def test_collector_msg_value():
@@ -22,27 +25,28 @@ def test_collector_msg_value():
         del collector
 
         new_file = temp_dir + '/current'
-        with open(new_file, 'r') as f:
-            last_string = f.readline()
-        assert check_msg_value in last_string
+        with open(new_file, 'rb') as f:
+            last_string = f.read()
+        assert last_string == b"  " + check_msg_value
 
 
-def test_collector_rollback():
+def test_collector_rollover():
     with tempfile.TemporaryDirectory() as temp_dir:
-        collector = Collector(temp_dir, 5)
+        collector = Collector(temp_dir, roll_after=5)
         for i in range(9):
             collector.process_raw_message((raw_msg + str(i)).encode())
 
-        # Force the buffer to write to the dile by removing the open hadle
         del collector
 
         new_file = temp_dir + '/current'
-        with open(new_file, 'r') as f:
-            f0_last_string = f.readline()
-        test_dir = temp_dir + '/'
-        new_file = glob.glob(os.path.join(test_dir, '16*0'))[0]
-        with open(new_file, 'r') as f:
-            f1_last_string = f.readline()
+        with open(new_file, 'rb') as f:
+            f0_last_string = f.read()
+
+        files = [x for x in os.listdir(temp_dir) if x.endswith("_0")]
+        assert len(files) == 1, "could not find a unique rolled collector file"
+        new_file = os.path.join(temp_dir, files[0])
+        with open(new_file, 'rb') as f:
+            f1_last_string = f.read()
         assert check_msg_rollback in f0_last_string
         assert check_msg_value in f1_last_string
 
@@ -52,7 +56,6 @@ def test_rotating_file():
         collector = Collector(temp_dir)
         collector.process_raw_message((raw_msg).encode())
 
-        # Force the buffer to write to the file by removing the open handle
         del collector
 
         coll_curr_start_time = os.path.join(temp_dir, 'current_start_time')
@@ -111,3 +114,40 @@ def test_use_json():
         del collector
         with open(os.path.join(temp_dir, "current"), "r") as fin:
             assert fin.read() == json.dumps(test_dict) + "\n"
+
+
+def test_collector_cli_defaults():
+    opts = docopt(main.__doc__, ["collector"])
+    with patch("asyncio.get_event_loop"), patch(
+        "mite.cli.collector._collector_receiver"
+    ) as receiver_mock:
+        c = cli_collector.collector(opts)
+    c = receiver_mock.return_value.add_raw_listener.call_args_list[0][0][0].__self__
+    assert c._filter_fn is None
+    assert not c._use_json
+    assert c._roll_after == 100000
+    assert c._target_dir == os.path.abspath("collector_data")
+
+
+def test_collector_cli_use_json():
+    opts = docopt(main.__doc__, ["collector", "--collector-use-json"])
+    with patch("asyncio.get_event_loop"), patch(
+        "mite.cli.collector._collector_receiver"
+    ) as receiver_mock:
+        cli_collector.collector(opts)
+    c = receiver_mock.return_value.add_raw_listener.call_args_list[0][0][0].__self__
+    assert c._use_json
+
+
+def foo():
+    pass
+
+
+def test_collector_cli_filter():
+    opts = docopt(main.__doc__, ["collector", "--collector-filter=test_collector:foo"])
+    with patch("asyncio.get_event_loop"), patch(
+        "mite.cli.collector._collector_receiver"
+    ) as receiver_mock:
+        cli_collector.collector(opts)
+    c = receiver_mock.return_value.add_raw_listener.call_args_list[0][0][0].__self__
+    assert c._filter_fn is foo
