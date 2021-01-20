10631010177146085e88f99ddbff1771a8013cce
Matt Ellis <matthew.ellis@sky.uk>
2021-01-20 13:27:52 +0000

    Removed assertions duplicated in another test

--- acurl/tests/test_httpbin.py
+++ acurl/tests/test_httpbin.py
@@ -43,9 +43,6 @@ async def test_session_cookies(httpbin):
 async def test_session_cookies_sent_on_subsequent_request(httpbin):
     s = session()
     await s.get(httpbin.url + "/cookies/set?name=value")
-    cookie_list = s.get_cookie_list()
-    assert len(cookie_list) == 1
-    assert cookie_list[0].name == "name"
     resp = await s.get(httpbin.url + "/cookies")
     data = resp.json()
     assert len(data) == 1
