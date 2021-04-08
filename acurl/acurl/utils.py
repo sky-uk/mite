7b60087498a4a52d1df465e6e962ba15f61f3e46
Aaron Ecay <aaronecay@gmail.com>
2021-04-08 10:27:58 +0100

    fix

--- acurl/acurl/utils.py
+++ acurl/acurl/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import MutableMapping
+from collections.abc import MutableMapping
 
 
 class _CaseInsensitiveDict(MutableMapping):
