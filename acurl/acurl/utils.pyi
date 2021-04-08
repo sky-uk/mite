ba14ec3e661543e35aa20aed5ec1ed2889b659cf
Aaron Ecay <aaronecay@gmail.com>
2021-04-08 10:24:51 +0100

    fix

--- acurl/acurl/utils.pyi
+++ acurl/acurl/utils.pyi
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import ABCMeta
 from collections import defaultdict
-from typing import MutableMapping, TypeVar
+from typing import DefaultDict, Dict, MutableMapping, TypeVar
 
 _K = TypeVar("_K")
 _V = TypeVar("_V")
@@ -12,8 +12,8 @@ class _CaseInsensitiveDict(MutableMapping[_K, _V], metaclass=ABCMeta):
     def _k(key: _K) -> _K: ...
     def _convert_keys(self) -> None: ...
 
-class CaseInsensitiveDict(_CaseInsensitiveDict[_K, _V], dict[_K, _V]):
+class CaseInsensitiveDict(_CaseInsensitiveDict[_K, _V], Dict[_K, _V]):
     ...
 
-class CaseInsensitiveDefaultDict(_CaseInsensitiveDict[_K, _V], defaultdict[_K, _V]):
+class CaseInsensitiveDefaultDict(_CaseInsensitiveDict[_K, _V], Defaultdict[_K, _V]):
     ...
