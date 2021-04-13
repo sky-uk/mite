a51bdffc75d3a3697e16a1bb9f6e2fbd6d538cf5
Matt Ellis <matthew.ellis@sky.uk>
2021-04-13 15:19:35 +0100

    Adds wait_for_elements method to mite selenium (#192)
    * Adds wait_for_elements method to mite selenium

--- mite_selenium/__init__.py
+++ mite_selenium/__init__.py
@@ -153,11 +153,15 @@ class _SeleniumWrapper:
     def get_js_metrics_context(self):
         return JsMetricsContext(self)
 
+    def wait_for_elements(self, locator, timeout=5):
+        return self._wait_for(EC.presence_of_all_elements_located, locator, timeout)
+
     def wait_for_element(self, locator, timeout=5):
+        return self._wait_for(EC.presence_of_element_located, locator, timeout)
+
+    def _wait_for(self, condition_func, locator, timeout=5):
         try:
-            return WebDriverWait(self._remote, timeout).until(
-                EC.presence_of_element_located(locator)
-            )
+            return WebDriverWait(self._remote, timeout).until(condition_func(locator))
         except TimeoutException as te:
             raise MiteError(
                 f"Timed out trying to find element '{locator}' in the dom"
