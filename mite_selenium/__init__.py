0bdf97176ee2802c95501c4f3822e1cb39c586d4
Matt Ellis <matthew.ellis@sky.uk>
2021-04-13 11:54:01 +0100

    Refactored to be a single method

--- mite_selenium/__init__.py
+++ mite_selenium/__init__.py
@@ -154,19 +154,13 @@ class _SeleniumWrapper:
         return JsMetricsContext(self)
 
     def wait_for_elements(self, locator, timeout=5):
-        return self._wait_for(EC.presence_of_all_elements_located, locator, timeout)
-
-    def wait_for_element(self, locator, timeout=5):
-        return self._wait_for(EC.presence_of_element_located, locator, timeout)
-
-    def _wait_for(self, condition_func, locator, timeout=5):
         try:
             return WebDriverWait(self._remote, timeout).until(
-                condition_func(locator)
+                EC.presence_of_all_elements_located(locator, timeout)
             )
         except TimeoutException as te:
             raise MiteError(
-                f"Timed out trying to find element '{locator}' in the dom"
+                f"Timed out trying to find element(s) '{locator}' in the dom"
             ) from te
 
     def switch_to_iframe(self, locator):
