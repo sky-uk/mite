f633217aa352c4dd7f1312ecbe8d4f87e509e553
john horton <john.horton2@sky.uk>
2021-10-27 11:01:13 +0100

    using pip for install and dependency rather than tox, then using tox to run tests

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -7,25 +7,7 @@ if [ $PRE_COMMIT_STATUS -ne 0 ]; then
     git diff
 fi
 
-pip install -r test-requirements.txt
-pip install -r requirements.txt
-       
-pip install -e ./acurl --user
-pip install -e ./acurl_ng --user
-
-TEST_EXIT_CODE=1
-pytest acurl/tests
-EC1=$?
-pytest acurl_ng/tests
-EC2=$?
-
-if [ "$EC1" -eq 0 -a "$EC2" -eq 0 ];
-then
-    TEST_EXIT_CODE=0
-fi
-
-# tox seems to be giving issues, so dropping it for now
-# tox -e py39; TOX_EXIT_CODE=$?
+tox -e py39; TOX_EXIT_CODE=$?
 
 # Further ideas for jobs to run:
 # - license check
@@ -36,9 +18,4 @@ fi
 # - documentation coverage
 # - docs build (on master only)
 
-# when we start using tox again, uncomment the line below
-# [ "$TOX_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit 1
-
-# remove the line below once (if) we start using tox again
-[ "$TEST_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit
-
+[ "$TOX_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit 1
