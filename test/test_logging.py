5a8773b6b7409e8757ea668907c4ae2dab740312
john horton <john.horton2@sky.uk>
2021-11-04 15:08:19 +0000

    fixed black issues

--- test/test_logging.py
+++ test/test_logging.py
@@ -2,15 +2,11 @@ import pytest
 import logging
 from mite.__main__ import setup_logging
 
+
 def test_journey_logging():
-    optsTrue = {
-        "--log-level": "DEBUG",
-        "--journey-logging": ""
-    }
- 
-    optsFalse = {
-        "--log-level": "DEBUG"
-    }
+    optsTrue = {"--log-level": "DEBUG", "--journey-logging": ""}
+
+    optsFalse = {"--log-level": "DEBUG"}
 
     setup_logging(optsTrue)
     assert logging.__dict__.get("journey_logging") == True
