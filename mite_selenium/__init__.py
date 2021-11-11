70094ef702c4700db12dfedfd1dac0b4bca641ac
Haffi Mazhar <haffimazhae96@gmail.com>
2021-11-11 10:09:52 +0000

    headless

--- mite_selenium/__init__.py
+++ mite_selenium/__init__.py
@@ -31,7 +31,7 @@ class _SeleniumWrapper:
         self._file_detector = self._spec_import_if_not_none("webdriver_file_detector")
         self._proxy = self._spec_import_if_not_none("webdriver_proxy")
         self._browser_profile = self._spec_import_if_not_none("webdriver_browser_profile")
-        self._options = self._spec_import_if_not_none("webdriver_options")
+        self._options = self._context.config.get("webdriver_options")
 
         # Required param
         self._capabilities = self._context.config.get("webdriver_capabilities")
