d37bde2908a3c641c826c3bd82b66f8416abaa97
Purcherea, Cosmin <cosmin.purcherea@sky.uk>
2021-11-12 12:04:25 +0000

    Check AcurlError message test

--- acurl_ng/tests/test_acurl_ng.py
+++ acurl_ng/tests/test_acurl_ng.py
@@ -8,6 +8,8 @@ import acurl_ng
 # https://everything.curl.dev/usingcurl/returns
 @pytest.mark.asyncio
 async def test_non_zero_exit_code_raises(acurl_session_ng):
-    with pytest.raises(acurl_ng.AcurlError):
+    with pytest.raises(
+        acurl_ng.AcurlError, match="curl failed with code 6 Couldn't resolve host name"
+    ):
         # Curl returns CURLE_COULDNT_RESOLVE_HOST (6) exit code
         await acurl_session_ng.get("unresolvable_hostname.doesnotexist")
