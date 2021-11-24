33c4eff86680200d2b181dd4f0864527e1e591bb
Ryan Linnit <ryan.linnit@sky.uk>
2021-11-24 13:40:25 +0000

    Removed test_response_headers_with_multiple_HTTP_100 test

--- acurl_ng/tests/test_response_ng.py
+++ acurl_ng/tests/test_response_ng.py
@@ -1,11 +1,6 @@
-import asyncio
-from functools import partial
-
 import pytest
 from werkzeug.datastructures import Headers
 from werkzeug.wrappers import Response
-
-import acurl_ng
 
 
 @pytest.mark.asyncio
@@ -47,39 +42,6 @@ async def test_response_headers_with_HTTP_100(httpserver, acurl_session_ng):
 
     assert "Foo" in r.headers
     assert r.headers["Foo"] == "bar"
-
-
-# FIXME: This test hangs after the introduction of AcurlError handling because curl returns CURLE_RECV_ERROR (56)
-@pytest.mark.skip
-async def test_response_headers_with_multiple_HTTP_100(acurl_session_ng):
-    # It's unclear if this can happen. It doesn't sound like it should, but
-    # there's documentation of it happening in IIS at least:
-    # https://stackoverflow.com/questions/22818059/several-100-continue-received-from-the-server
-    body = b"".join(
-        (
-            b"HTTP/1.1 100 Continue\r\n",
-            b"\r\n",
-            b"HTTP/1.1 100 Continue\r\n",
-            b"\r\n",
-            b"HTTP/1.1 200 OK\r\n",
-            b"Foo: bar\r\n",
-            b"\r\n",
-        )
-    )
-    server = await asyncio.start_server(
-        partial(connected_cb, body), host="127.0.0.1", port=10764
-    )
-    await server.start_serving()
-
-    async def go():
-        r = await acurl_session_ng.get("http://localhost:10764/foo")
-        server.close()
-        return r
-
-    results = await asyncio.gather(go(), server.serve_forever(), return_exceptions=True)
-    resp = [x for x in results if isinstance(x, acurl_ng._Response)][0]
-    assert "Foo" in resp.headers
-    assert resp.headers["Foo"] == "bar"
 
 
 @pytest.mark.asyncio
