95d1422cca56b2bd6c63a96fefa2c25bdbbfc61a
Ryan Linnit <ryan.linnit@sky.uk>
2022-05-20 15:48:46 +0100

    proper scenario

--- testjourney.py
+++ testjourney.py
@@ -1,24 +1,30 @@
-
-import asyncio
 from mite_http import mite_http
 from mite import ensure_fixed_separation
+from mite.scenario import StopScenario
 
 @mite_http
 async def journey(ctx):
     async with ctx.transaction('test1'):
         async with ensure_fixed_separation(1):
-            ctx.send("test_message")
-            await asyncio.sleep(0.5)
-            resp = await ctx.http.get('http://localhost:8000')
-        # breakpoint()
-
-        return resp
+            await ctx.http.get('http://localhost:8000')
 
 
-volumemodel = lambda start, end: 10
+def volume_model_factory(n, duration=60 * 5):
+    def vm(start, end):
+        if start > duration:
+            raise StopScenario
+        return n
+
+    vm.__name__ = f"volume model {n}"
+    return vm
 
 
-def scenario():
-    return [
-        ['mite.example:journey', None, volumemodel],
-    ]
+scenarios = [
+    (10, "testjourney:journey", None),
+]
+
+
+# Peak scenario running at full TPS for 1 hour
+def peak_scenario():
+    for peak, journey, datapool in scenarios:
+        yield journey, datapool, volume_model_factory(peak, duration=1 * 60 * 60)
