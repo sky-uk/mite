eb3047db3bccea9386295ce58d71c36ae12d3a32
Ryan Linnit <ryan.linnit@sky.uk>
2022-05-22 19:47:30 +0100

    added test message journey

--- testjourney.py
+++ testjourney.py
@@ -4,9 +4,14 @@ from mite.scenario import StopScenario
 
 @mite_http
 async def journey(ctx):
-    async with ctx.transaction('test1'):
+    async with ctx.transaction("test1"):
         async with ensure_fixed_separation(1):
-            await ctx.http.get('http://localhost:8000')
+            await ctx.http.get("http://localhost:8000")
+
+async def message_journey(ctx):
+    async with ctx.transaction("test2"):
+        async with ensure_fixed_separation(1):
+            ctx.send("test_message")
 
 
 def volume_model_factory(n, duration=60 * 5):
@@ -23,8 +28,16 @@ scenarios = [
     (10, "testjourney:journey", None),
 ]
 
+message_scenarios = [
+    (10, "testjourney:message_journey", None),
+]
+
 
 # Peak scenario running at full TPS for 1 hour
 def peak_scenario():
     for peak, journey, datapool in scenarios:
-        yield journey, datapool, volume_model_factory(peak, duration=1 * 60 * 60)
+        yield journey, datapool, volume_model_factory(peak, duration=12 * 60 * 60)
+
+def message_scenario():
+    for peak, journey, datapool in message_scenarios:
+        yield journey, datapool, volume_model_factory(peak, duration=12 * 60 * 60)
