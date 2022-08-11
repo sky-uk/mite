a3aa470291d5085459bcace656e203d7c62db7a7
Ryan Linnit <ryan.linnit@sky.uk>
2022-08-11 09:12:43 +0100

    tidying up

--- cd-scripts/cdRelease.sh
+++ cd-scripts/cdRelease.sh
@@ -2,7 +2,6 @@
 
 VERSION=""
 
-#get parameters
 while getopts v: flag
 do
   case "${flag}" in
@@ -10,29 +9,19 @@ do
   esac
 done
 
-#get highest tag number, and add 1.0.0 if doesn't exist
 CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
 
 if [[ $CURRENT_VERSION == "" ]]
 then
   CURRENT_VERSION="v1.0.0"
 fi
-# echo "Current Version: $CURRENT_VERSION"
 
-
-#replace . with space so can split into an array
+# Replace . with space so can split into an array
 CURRENT_VERSION_PARTS=($(echo $CURRENT_VERSION | sed -E 's/v([0-9]+)\.([0-9]+)\.?([0-9]+)?/\1 \2 \3/'))
 
-#get number parts
 MAJOR=${CURRENT_VERSION_PARTS[0]}
 MINOR=${CURRENT_VERSION_PARTS[1]}
 PATCH=${CURRENT_VERSION_PARTS[2]}
-
-# echo "Current: $MAJOR $MINOR $PATCH"
-
-# echo $((MAJOR+1))
-
-# exit 0
 
 if [[ $VERSION == "major" ]]
 then
@@ -48,8 +37,6 @@ else
   exit 1
 fi
 
-
-#create new tag
 NEW_TAG="v$MAJOR.$MINOR.$PATCH"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
@@ -57,11 +44,9 @@ GIT_COMMIT=$(git rev-parse HEAD)
 NEEDS_TAG=$(git describe --contains $GIT_COMMIT 2>/dev/null)
 
 if [ -z "$NEEDS_TAG" ]; then
-    echo "TAGGING $NEW_TAG"
-    git tag $NEW_TAG
-    git push --tags
-else
-    echo "ALREADY HAS TAG"
+  echo "Creating git tag: $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
 fi
 
 exit 0 
