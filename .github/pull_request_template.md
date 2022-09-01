46a7f4e0b7464fad6374d073c1d2005538e675c5
Ryan Linnit <85644054+ryanlinnit-sky@users.noreply.github.com>
2022-09-01 09:33:40 +0100

    Better versioning (#246)

---
+++ .github/pull_request_template.md
@@ -0,0 +1,8 @@
+
+#### What is the change?
+
+#### Does this change require a version increment:
+
+- [ ] Major
+- [ ] Minor
+- [x] Patch
--- cd-scripts/cdRelease.py
+++ cd-scripts/cdRelease.py
@@ -1,9 +1,10 @@
 """cdRelease
 
 Usage:
-    cdRelease.py [options] patch
-    cdRelease.py [options] minor
     cdRelease.py [options] major
+    cdRelease.py [options] minor
+    cdRelease.py [options] patch
+    cdRelease.py [options] [--pr_number=<pr_number>]
 
     cdRelease.py (-h | --help)
 
@@ -13,16 +14,40 @@ Options:
 """
 
 import logging
+import re
 import sys
 
 import docopt
 import git
+import requests
 from packaging.version import parse
 
 logger = logging.getLogger("cdRelease")
 
 
-def increment_version(clv, opts):
+def increment_version_from_pr(clv, version_parts_to_increment):
+    major = clv.major
+    minor = clv.minor
+    patch = clv.micro
+
+    if "major" in version_parts_to_increment:
+        major += 1
+        minor = 0
+        patch = 0
+    elif "minor" in version_parts_to_increment:
+        minor += 1
+        patch = 0
+    elif "patch" in version_parts_to_increment:
+        patch += 1
+    else:
+        raise ValueError(
+            f"Does not contain a valid specification of a version to increment: {version_parts_to_increment}"
+        )
+
+    return f"v{major}.{minor}.{patch}"
+
+
+def increment_version_manually(clv, opts):
     if opts["major"]:
         major = clv.major + 1
         minor = 0
@@ -55,6 +80,21 @@ def create_and_push_tag(repo, tag):
         sys.exit(1)
 
 
+def parse_pr(opts):
+    resp = requests.get(
+        f"https://api.github.com/repos/sky-uk/mite/pulls/{opts['--pr_number']}"
+    )
+    pr_message = resp.json()["body"]
+
+    matches = re.findall(
+        r"^- \[x\] (major|minor|patch)\r$", pr_message, re.IGNORECASE | re.MULTILINE
+    )
+
+    # Return a lowercase list, in case the user has changed the case during
+    # the creation of their pull request
+    return [m.lower() for m in matches if m.lower() in ("major", "minor", "patch")]
+
+
 def main():
     opts = docopt.docopt(__doc__)
     logging.basicConfig(
@@ -78,7 +118,19 @@ def main():
         latest_tag = "v0.0.0"
 
     current_latest_version = parse(latest_tag)
-    new_tag = increment_version(current_latest_version, opts)
+
+    if opts["--pr_number"]:
+        version_parts_to_increment = parse_pr(opts)
+        if not version_parts_to_increment:
+            logger.info("No release")
+            with open("/tmp/workspace/env_vars", "a") as f:
+                f.write("export VERSION_INCREMENT=false")
+            sys.exit(0)
+        new_tag = increment_version_from_pr(
+            current_latest_version, version_parts_to_increment
+        )
+    else:
+        new_tag = increment_version_manually(current_latest_version, opts)
 
     create_and_push_tag(repo, new_tag)
 
