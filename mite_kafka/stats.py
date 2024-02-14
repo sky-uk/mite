98dfa660b4cbb6e6a4fc0f17b241bdea2fb5d8df
josephantonycj <joseph.jeffrey@sky.uk>
2024-02-14 16:51:38 +0000

    Updated the stats

--- mite_kafka/stats.py
+++ mite_kafka/stats.py
@@ -1,42 +1,27 @@
-from mite.stats import Accumulator, Extractor, extractor, matcher_by_type
+from mite.stats import Accumulator, Extractor, matcher_by_type
 
 # Kafka
 
 
 def _kafka_extract(msg):
-    for key, value in msg["total_received"].items():
-        yield (key, msg["topic"]), len(value)
-
-
-def _websocket_extract(msg):
-    for key, value in msg["message"].items():
-        yield (key, msg["topic"]), len(value)
+    for key, value in msg["metrics"].items():
+        yield (key, msg["topic"]), value
 
 
 _KAFKA_STATS = [
     Accumulator(
-        "mite_kafka_tx_stats",
-        matcher_by_type("kafka_tx_stats"),
-        extractor(["message_name", "write_only", "topic", "event_name"], "total_sent"),
+        "mite_kafka_producer_stats",
+        matcher_by_type("kafka_producer_stats"),
+        Extractor(labels=["metric_name", "topic"], extract=_kafka_extract),
     ),
     Accumulator(
-        "mite_kafka_rx_stats",
-        matcher_by_type("kafka_rx_stats"),
-        Extractor(labels=["message_name", "topic"], extract=_kafka_extract),
+        "mite_kafka_consumer_stats",
+        matcher_by_type("kafka_consumer_stats"),
+        Extractor(labels=["metric_name", "topic"], extract=_kafka_extract),
     ),
     Accumulator(
-        "mite_kafka_external_rx_stats",
-        matcher_by_type("kafka_external_rx_stats"),
-        Extractor(labels=["message_name", "topic"], extract=_kafka_extract),
-    ),
-    Accumulator(
-        "mite_websocket_recv_stats",
-        matcher_by_type("wb_recv_stats"),
-        Extractor(labels=["message_name", "topic"], extract=_websocket_extract),
-    ),
-    Accumulator(
-        "mite_websocket_recv_external_stats",
-        matcher_by_type("wb_recv_external_stats"),
-        Extractor(labels=["message_name", "topic"], extract=_websocket_extract),
+        "mite_kafka_topic_stats",
+        matcher_by_type("kafka_topic_stats"),
+        Extractor(labels=["metric_name", "topic"], extract=_kafka_extract),
     ),
 ]
