de397d39a52deaf3db900fab5b1f62404a134e85
josephantonycj <joseph.jeffrey@sky.uk>
2024-03-01 16:18:08 +0000

    changes to unit test

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -3,30 +3,24 @@ from unittest.mock import AsyncMock, patch
 
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 import pytest
-from mocks.mock_context import MockContext
-
 from mite_kafka import _KafkaWrapper, mite_kafka
 
-
-@pytest.fixture
-def mock_context():
-    context = MockContext()
-    context.kafka = _KafkaWrapper()  # Assign _KafkaWrapper instance to the kafka attribute
-    return context
+class MockContext:
+    pass
 
 @pytest.mark.asyncio
-async def test_mite_kafka_decorator(mock_context):
-    context = mock_context
+async def test_mite_kafka_decorator():
+    context = MockContext()
 
     @mite_kafka
     async def dummy_journey(ctx):
-        assert ctx.kafka is not None
+        assert hasattr(ctx, "kafka")
 
     await dummy_journey(context)
 
 @pytest.mark.asyncio
-async def test_mite_kafka_decorator_uninstall(mock_context):
-    context = mock_context
+async def test_mite_kafka_decorator_uninstall():
+    context = MockContext()
 
     @mite_kafka
     async def dummy_journey(ctx):
@@ -34,42 +28,42 @@ async def test_mite_kafka_decorator_uninstall(mock_context):
 
     await dummy_journey(context)
 
-    assert getattr(context, "kafka", None) is None
+    assert not hasattr(context, "kafka")
 
 @pytest.mark.asyncio
-async def test_mite_connect_producer(mock_context):
-    context = mock_context
+async def test_mite_connect_producer():
+    context = MockContext()
     url = "kafka://foo.bar"
 
     connect_mock = AsyncMock()
 
     with patch("mite_kafka._KafkaWrapper.create_producer", new=connect_mock):
-        await context.kafka.connect_producer(url)
+        await _KafkaWrapper().create_producer(url)
 
     connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
 
 @pytest.mark.asyncio
-async def test_mite_connect_consumer(mock_context):
-    context = mock_context
+async def test_mite_connect_consumer():
+    context = MockContext()
     url = "kafka://foo.bar"
 
     connect_mock = AsyncMock()
 
     with patch("mite_kafka._KafkaWrapper.create_consumer", new=connect_mock):
-        await context.kafka.connect_consumer(url)
+        await _KafkaWrapper().create_consumer(url)
 
     connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
 
 @pytest.mark.asyncio
-async def test_kafka_produce_message(mock_context):
-    context = mock_context
-    producer = await context.kafka.create_producer()
-    m = await context.kafka.send_and_wait(producer, "my_topic", b"hi")
+async def test_kafka_produce_message():
+    w = _KafkaWrapper()
+    producer = await w.create_producer()
+    m = await w.send_and_wait(producer, "my_topic", key=b"key", value=b"hi")
     assert m is not None
 
 @pytest.mark.asyncio
-async def test_kafka_consume_message(mock_context):
-    context = mock_context
-    consumer = await context.kafka.create_consumer()
-    m = await context.kafka.get_message(consumer, "my_topic")
+async def test_kafka_consume_message():
+    w = _KafkaWrapper()
+    consumer = await w.create_consumer()
+    m = await w.get_message(consumer, "my_topic")
     assert m is not None
