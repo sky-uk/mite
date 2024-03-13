6f33a3c8ee49d1874829985c533e9e8d0343054e
josephantonycj <joseph.jeffrey@sky.uk>
2024-03-13 12:20:59 +0000

    updated test suite

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -28,35 +28,3 @@ async def test_mite_kafka_decorator_uninstall():
     assert getattr(context, "kafka", None) is None
 
 
-@pytest.mark.asyncio
-async def test_mite_connect_producer():
-    context = MockContext()
-    url = "kafka://foo.bar"
-
-    connect_mock = AsyncMock()
-
-    @mite_kafka
-    async def dummy_journey(ctx):
-        await ctx.kafka.create_producer(url)
-
-    with patch("mite_kafka._KafkaWrapper.create_producer", new=connect_mock) as create_producer_mock:
-        await dummy_journey(context)
-
-    create_producer_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
-
-
-@pytest.mark.asyncio
-async def test_mite_connect_consumer():
-    context = MockContext()
-    url = "kafka://foo.bar"
-
-    connect_mock = AsyncMock()
-
-    @mite_kafka
-    async def dummy_journey(ctx):
-        await ctx.kafka.create_consumer(url)
-
-    with patch("mite_kafka._KafkaWrapper.create_consumer", new=connect_mock) as create_consumer_mock:
-        await dummy_journey(context)
-
-    create_consumer_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
