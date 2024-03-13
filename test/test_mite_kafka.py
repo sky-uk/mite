684f60c0d66f432a30cbf3d22fecd1a8ae2fd9e6
josephantonycj <joseph.jeffrey@sky.uk>
2024-03-13 11:06:56 +0000

    updated test suite

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -1,57 +1,64 @@
+import asyncio
+from unittest.mock import AsyncMock, patch
+
 import pytest
-from unittest.mock import MagicMock
-from mite_kafka import _KafkaWrapper, KafkaError, _kafka_context_manager
-
-@pytest.fixture
-def kafka_wrapper():
-    return _KafkaWrapper()
+from mite_kafka import _KafkaWrapper, mite_kafka
+from mocks.mock_context import MockContext
 
 @pytest.mark.asyncio
-async def test_create_producer(kafka_wrapper):
-    producer = await kafka_wrapper.create_producer(bootstrap_servers='localhost:9092')
-    assert producer is not None
+async def test_mite_kafka_decorator():
+    context = MockContext()
+
+    @mite_kafka
+    async def dummy_journey(ctx):
+        assert ctx.kafka is not None
+
+    await dummy_journey(context)
 
 @pytest.mark.asyncio
-async def test_create_consumer(kafka_wrapper):
-    consumer = await kafka_wrapper.create_consumer(bootstrap_servers='localhost:9092', group_id='test_group')
-    assert consumer is not None
+async def test_mite_kafka_decorator_uninstall():
+    context = MockContext()
 
-@pytest.mark.asyncio
-async def test_send_and_wait(kafka_wrapper):
-    producer_mock = MagicMock()
-    await kafka_wrapper.send_and_wait(producer_mock, 'test_topic', key='test_key', value='test_value')
-    producer_mock.start.assert_called_once()
-    producer_mock.send_and_wait.assert_called_once_with('test_topic', key='test_key', value='test_value')
-    producer_mock.stop.assert_called_once()
-
-@pytest.mark.asyncio
-async def test_get_message(kafka_wrapper):
-    consumer_mock = MagicMock()
-    consumer_mock.__aiter__.return_value = iter([MagicMock()])
-    message = await kafka_wrapper.get_message(consumer_mock, 'test_topic')
-    assert message is not None
-    consumer_mock.start.assert_called_once()
-    consumer_mock.stop.assert_called_once()
-
-@pytest.mark.asyncio
-async def test_create_message(kafka_wrapper):
-    value = 'test_value'
-    message = await kafka_wrapper.create_message(value)
-    assert message == value
-
-@pytest.mark.asyncio
-async def test_kafka_context_manager_success():
-    context = MagicMock()
-    async with _kafka_context_manager(context):
+    @mite_kafka
+    async def dummy_journey(ctx):
         pass
-    context.kafka.install.assert_called_once()
-    context.kafka.uninstall.assert_called_once()
+
+    await dummy_journey(context)
+
+    assert getattr(context, "kafka", None) is None
+
 
 @pytest.mark.asyncio
-async def test_kafka_context_manager_exception():
-    context = MagicMock()
-    with pytest.raises(KafkaError):
-        async with _kafka_context_manager(context):
-            raise ValueError("Test error")
-    context.kafka.install.assert_called_once()
-    context.kafka.uninstall.assert_called_once()
+async def test_mite_connect_producer():
+    context = MockContext()
+    url = "kafka://foo.bar"
+
+    connect_mock = AsyncMock()
+
+    @mite_kafka
+    async def dummy_journey(ctx):
+        await ctx.kafka.create_producer(url)
+
+    with patch("mite_kafka._KafkaWrapper.create_producer", new=connect_mock):
+        await dummy_journey(context)
+
+    connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
+
+
+@pytest.mark.asyncio
+async def test_mite_connect_consumer():
+    context = MockContext()
+    url = "kafka://foo.bar"
+
+    connect_mock = AsyncMock()
+
+    @mite_kafka
+    async def dummy_journey(ctx):
+        await ctx.kafka.create_consumer(url)
+
+    with patch("mite_kafka._KafkaWrapper.create_consumer", new=connect_mock):
+        await dummy_journey(context)
+
+    connect_mock.assert_called_once_with(url, loop=asyncio.get_event_loop())
+
+
