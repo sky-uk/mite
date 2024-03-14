40c7bc78e5b00a9ed1f345af6b16d568c62030c7
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2024-03-14 09:39:43 +0000

    remove unused import

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -4,7 +4,6 @@ from unittest.mock import AsyncMock, patch
 import pytest
 from mite_kafka import _KafkaWrapper, mite_kafka
 from mocks.mock_context import MockContext
-from aiokafka.producer import AIOKafkaProducer
 
 @pytest.mark.asyncio
 async def test_mite_kafka_decorator():
