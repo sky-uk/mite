431f832da9fa3d22aba54446d6566885d19cf504
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2024-03-19 10:50:22 +0000

    producer test

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -4,7 +4,7 @@ from unittest.mock import AsyncMock, patch
 import pytest
 from mite_kafka import _KafkaWrapper, mite_kafka
 from mocks.mock_context import MockContext
-from aiokafka import AIOKafkaProducer
+from aiokafka.producer import AIOKafkaProducer
 
 @pytest.mark.asyncio
 async def test_mite_kafka_decorator():
@@ -27,6 +27,23 @@ async def test_mite_kafka_decorator_uninstall():
     await dummy_journey(context)
 
     assert getattr(context, "kafka", None) is None
+
+@pytest.mark.asyncio
+async def test_kafka_producer():
+    producer_mock = AsyncMock()
+    kafka_wrapper = _KafkaWrapper()
+    with patch.object("aiokafka.producer.AIOKafkaProducer", return_value=producer_mock) as mocked:
+        async def dummy(*d, **kw):
+            return
+
+        mocked.side_effect = dummy
+        await kafka_wrapper.create_producer(bootstrap_servers='broker_url')
+        await kafka_wrapper.send_and_wait(b"some_topic", b"hello")
+
+        AIOKafkaProducer.assert_called_once_with(
+            bootstrap_servers='broker_url',
+            loop=asyncio.get_event_loop())
+     
 
 
         
