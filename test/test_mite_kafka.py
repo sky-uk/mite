587220bfa2dad95bbe95e2457edb48443fc8c507
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2024-03-22 09:17:00 +0000

    testing send method

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -38,9 +38,9 @@ async def test_producer():
         # Create an instance of _KafkaWrapper
         kafka_wrapper = _KafkaWrapper()
         # Call the create_producer method
-        await kafka_wrapper.create_producer(bootstrap_servers='broker_url')  # Pass the broker URL as a keyword argument
+        producer = await kafka_wrapper.create_producer(bootstrap_servers='broker_url')  # Pass the broker URL as a keyword argument
         # Call the send_and_wait method
-        await kafka_wrapper.send_and_wait("some_topic", "Hello Kafka")
+        await kafka_wrapper.send_and_wait(producer, 'some_topic', 'Hello Kafka')
         # Assert that the AIOKafkaProducer class was called with the expected arguments
         await asyncio.sleep(0)
         producer_mock.assert_called_once_with()
