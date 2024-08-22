4f58738b427bdd1a036c47e67ae39a683217d6ed
Jordan Brennan <jordan.brennan@sky.uk>
2024-08-22 10:58:30 +0100

    Swap async mock for magic mock as what they're mocking is not actuall async

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -1,5 +1,5 @@
 import asyncio
-from unittest.mock import AsyncMock, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from mite_kafka import _KafkaWrapper, mite_kafka
@@ -33,7 +33,7 @@ async def test_mite_kafka_decorator_uninstall():
 @pytest.mark.asyncio
 async def test_create_producer():
     # Create a mock for AIOKafkaProducer
-    producer_mock = AsyncMock()
+    producer_mock = MagicMock()
 
     # Patch AIOKafkaProducer to return the mock
     with patch('aiokafka.producer.AIOKafkaProducer', new_callable=producer_mock):
@@ -42,13 +42,12 @@ async def test_create_producer():
         # Call the create_producer method
         await kafka_wrapper.create_producer(bootstrap_servers='broker_url')  # Pass the broker URL as a keyword argument
         # Assert that the AIOKafkaProducer class was called with the expected arguments
-        await asyncio.sleep(0)
         producer_mock.assert_called_once_with()
 
 @pytest.mark.asyncio
 async def test_create_consumer():
     # Create a mock for AIOKafkaConsumer
-    consumer_mock = AsyncMock()
+    consumer_mock = MagicMock()
 
     # Patch AIOKafkaConsumer to return the mock
     with patch('aiokafka.consumer.AIOKafkaConsumer', new_callable=consumer_mock):
@@ -57,6 +56,5 @@ async def test_create_consumer():
         # Call the create_consumer method
         await kafka_wrapper.create_consumer(bootstrap_servers='broker_url')  # Pass the broker URL as a keyword argument
         # Assert that the AIOKafkaConsumer class was called with the expected arguments
-        await asyncio.sleep(0)
         consumer_mock.assert_called_once_with()
         
