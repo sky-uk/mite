e1a90e0a4084f956f0afbedad063b77944afd7b5
Jenkins-CI <mite@noreply.github.com>
2024-11-08 16:36:52 +0000

    updates on tests

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -8,27 +8,28 @@ from mite_kafka import KafkaConsumer, KafkaProducer, mite_kafka
 
 @pytest.mark.asyncio
 async def test_mite_kafka_decorator():
-    context = MockContext()
+    mock_context = MockContext()
 
     @mite_kafka
     async def dummy_journey(ctx):
         assert ctx.kafka_producer is not None
         assert ctx.kafka_consumer is not None
 
-    await dummy_journey(context)
+    await dummy_journey(mock_context)
 
 
 @pytest.mark.asyncio
 async def test_mite_kafka_decorator_uninstall():
-    context = MockContext()
+    mock_context = MockContext()
 
     @mite_kafka
     async def dummy_journey(ctx):
         pass
 
-    await dummy_journey(context)
+    await dummy_journey(mock_context)
 
-    assert getattr(context, "kafka", None) is None
+    assert mock_context.kafka_producer is None
+    assert mock_context.kafka_consumer is None
 
 
 @pytest.mark.asyncio
@@ -37,14 +38,14 @@ async def test_create_producer():
     producer_mock = MagicMock()
 
     # Patch AIOKafkaProducer to return the mock
-    with patch("aiokafka.producer.AIOKafkaProducer", new_callable=producer_mock):
+    with patch("aiokafka.producer.AIOKafkaProducer", new_callable=lambda: producer_mock):
         # Create an instance of _KafkaWrapper
         kafka_wrapper = KafkaProducer()
         # Call the create_producer method
         await kafka_wrapper.create(bootstrap_servers="broker_url")
         # Pass the broker URL as a keyword argument
         # Assert that the AIOKafkaProducer class was called with the expected arguments
-        producer_mock.assert_called_once_with()
+        producer_mock.return_value.assert_called_once_with()
 
 
 @pytest.mark.asyncio
@@ -53,12 +54,10 @@ async def test_create_consumer():
     consumer_mock = MagicMock()
 
     # Patch AIOKafkaConsumer to return the mock
-    with patch("aiokafka.consumer.AIOKafkaConsumer", new_callable=consumer_mock):
+    with patch("aiokafka.consumer.AIOKafkaConsumer", new_callable=lambda: consumer_mock):
         # Create an instance of _KafkaWrapper
         kafka_wrapper = KafkaConsumer()
         # Call the create_consumer method
-        await kafka_wrapper.create(
-            bootstrap_servers="broker_url"
-        )  # Pass the broker URL as a keyword argument
+        await kafka_wrapper.create(bootstrap_servers="broker_url")
         # Assert that the AIOKafkaConsumer class was called with the expected arguments
-        consumer_mock.assert_called_once_with()
+        consumer_mock.return_value.assert_called_once_with()
