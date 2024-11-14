59de36a69ded78fe8ac72e53e87cfb4740d2249b
Jenkins-CI <mite@noreply.github.com>
2024-11-14 16:05:25 +0000

    test fixes

--- test/test_mite_kafka.py
+++ test/test_mite_kafka.py
@@ -36,13 +36,14 @@ async def test_mite_kafka_decorator_uninstall():
 async def test_create_producer():
     # Create a mock for AIOKafkaProducer
     producer_mock = MagicMock()
+    mock_context = MockContext()
 
     # Patch AIOKafkaProducer to return the mock
     with patch("aiokafka.producer.AIOKafkaProducer", new_callable=producer_mock):
         # Create an instance of _KafkaWrapper
-        kafka_wrapper = KafkaProducer()
+        kafka_wrapper = KafkaProducer(mock_context)
         # Call the create_producer method
-        await kafka_wrapper.create(bootstrap_servers="broker_url")
+        await kafka_wrapper.create_and_start(bootstrap_servers="broker_url")
         # Pass the broker URL as a keyword argument
         # Assert that the AIOKafkaProducer class was called with the expected arguments
         producer_mock.assert_called_once_with()
@@ -52,12 +53,13 @@ async def test_create_producer():
 async def test_create_consumer():
     # Create a mock for AIOKafkaConsumer
     consumer_mock = MagicMock()
+    mock_context = MockContext()
 
     # Patch AIOKafkaConsumer to return the mock
     with patch("aiokafka.consumer.AIOKafkaConsumer", new_callable=consumer_mock):
         # Create an instance of _KafkaWrapper
-        kafka_wrapper = KafkaConsumer()
+        kafka_wrapper = KafkaConsumer(mock_context)
         # Call the create_consumer method
-        await kafka_wrapper.create(bootstrap_servers="broker_url")
+        await kafka_wrapper.create_and_start(bootstrap_servers="broker_url")
         # Assert that the AIOKafkaConsumer class was called with the expected arguments
         consumer_mock.assert_called_once_with()
