67906be95613edd4c6004f56f81dddd6901f1aff
Purcherea, Cosmin <cosmin.purcherea@sky.uk>
2025-01-05 17:52:18 +0000

    see if it runs black ?

--- mite_kafka/__init__.py
+++ mite_kafka/__init__.py
@@ -1,12 +1,11 @@
 import asyncio
 from contextlib import asynccontextmanager
+from functools import wraps
 
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
-from functools import wraps
 
 
 class KafkaProducer:
-
     def __init__(self, ctx):
         self._loop = asyncio.get_event_loop()
         self._producer = None
@@ -64,12 +63,16 @@ class KafkaProducerSingleton(KafkaProducer):
 
     @classmethod
     async def get_instance(cls, context, singleton_id, *args, **kwargs):
+
         if singleton_id is None:
             singleton_id = "DEFAULT"
+
         if cls._instances.get(singleton_id, None) is None:
             cls._instances[f"{singleton_id}"] = cls(context, singleton_id)
             await cls._instances.get(singleton_id).create_and_start(*args, **kwargs)
+
         return cls._instances.get(singleton_id)
+
 
 @asynccontextmanager
 async def _kafka_context_manager(ctx):
@@ -83,38 +86,59 @@ async def _kafka_context_manager(ctx):
 
 
 @asynccontextmanager
-async def _kafka_singleton_context_manager(ctx, singleton_id, producer, consumer, *args, **kwargs):
+async def _kafka_singleton_context_manager(
+    ctx, singleton_id, producer, consumer, *args, **kwargs
+):
     if producer:
-        ctx.kafka_producer = await KafkaProducerSingleton.get_instance(ctx, singleton_id, *args, **kwargs)
+        ctx.kafka_producer = await KafkaProducerSingleton.get_instance(
+            ctx, singleton_id, *args, **kwargs
+        )
     if consumer:
         # TODO: Implement
         raise NotImplementedError
     yield
 
-def mite_kafka(*args, singleton=False, singleton_id=None, producer=False, consumer=False, bootstrap_servers=None, security_protocol=None, ssl_context=None, value_serializer=None):
+
+def mite_kafka(
+    *args,
+    singleton=False,
+    singleton_id=None,
+    producer=False,
+    consumer=False,
+    bootstrap_servers=None,
+    security_protocol=None,
+    ssl_context=None,
+    value_serializer=None,
+):
     def wrapper_factory(func):
         @wraps(func)
         async def wrapper(ctx, *args, **kwargs):
-            
+
             kafka_args = {}
             if bootstrap_servers:
-                kafka_args['bootstrap_servers'] = bootstrap_servers
+                kafka_args["bootstrap_servers"] = bootstrap_servers
             if security_protocol:
-                kafka_args['security_protocol'] = security_protocol
+                kafka_args["security_protocol"] = security_protocol
             if ssl_context:
-                kafka_args['ssl_context'] = ssl_context
+                kafka_args["ssl_context"] = ssl_context
             if value_serializer:
-                kafka_args['value_serializer'] = value_serializer
+                kafka_args["value_serializer"] = value_serializer
 
-            if singleton:    
-                async with _kafka_singleton_context_manager(ctx, singleton_id=singleton_id, producer=producer, consumer=consumer, **kafka_args):
+            if singleton:
+                async with _kafka_singleton_context_manager(
+                    ctx,
+                    singleton_id=singleton_id,
+                    producer=producer,
+                    consumer=consumer,
+                    **kafka_args,
+                ):
                     return await func(ctx, *args, **kwargs)
             else:
                 async with _kafka_context_manager(ctx):
                     return await func(ctx, *args, **kwargs)
 
         return wrapper
-    
+
     if len(args) == 0:
         # invoked as @mite_kafka(singleton=...) def foo(...)
         return wrapper_factory
