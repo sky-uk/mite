45e9e6078a2c86a02f7d595abe5fc9bdff374c82
Ryan Linnit <ryan.linnit@sky.uk>
2025-03-04 14:08:24 +0000

    add recyclable s3 datapool

--- mite/datapools.py
+++ mite/datapools.py
@@ -100,7 +100,12 @@ class SingleRunDataPoolWrapper:
 
 class S3IterableDataPool:
     def __init__(
-        self, bucket_name, prefix="", access_key_id=None, secret_access_key=None
+        self,
+        bucket_name,
+        prefix="",
+        access_key_id=None,
+        secret_access_key=None,
+        preload_minimum=None,
     ):
         self.s3_client = boto3.client(
             "s3",
@@ -112,27 +117,36 @@ class S3IterableDataPool:
         self.list_obj_kwargs = {"Bucket": self.bucket_name, "Prefix": self.prefix}
         self._data = []
         self.item_index = 0
+        self.populating = False
         self.exhausted = False
 
-        self.populate()
+        if preload_minimum:
+            while len(self._data) < preload_minimum and not self.exhausted:
+                self.populate()
+        else:
+            while not self.exhausted:
+                self.populate()
 
     def populate(self):
-        while not self.exhausted:
-            # print(f"{time.time()} calling list_objects_v2. len of data {len(self._data)}")
-            response = self.s3_client.list_objects_v2(**self.list_obj_kwargs)
+        response = self.s3_client.list_objects_v2(**self.list_obj_kwargs)
 
-            if response.get("IsTruncated"):
-                self.list_obj_kwargs.update(
-                    {"ContinuationToken": response["NextContinuationToken"]}
-                )
-            else:
-                self.exhausted = True
+        for obj in response["Contents"]:
+            self._data.append((self.item_index, obj["Key"]))
+            self.item_index += 1
 
-            for obj in response["Contents"]:
-                self.item_index += 1
-                self._data.append((self.item_index, obj["Key"]))
+        if response.get("IsTruncated"):
+            self.list_obj_kwargs.update(
+                {"ContinuationToken": response["NextContinuationToken"]}
+            )
+        else:
+            self.exhausted = True
 
     async def checkout(self, config):
+        if not self.exhausted and not self.populating:
+            self.populating = True
+            self.populate()
+            self.populating = False
+
         try:
             item_id, file_key = self._data.pop(0)
             file_obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=file_key)
@@ -145,3 +159,39 @@ class S3IterableDataPool:
 
     async def checkin(self, item_id):
         pass
+
+
+class S3RecyclableIterableDataPool(S3IterableDataPool):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._available = deque(range(len(self._data)))
+
+    async def checkout(self, config):
+        if not self.exhausted and not self.populating:
+            self.populating = True
+            starting_id = self.item_index
+            self.populate()
+            self._available.extend(range(starting_id, self.item_index))
+            self.populating = False
+
+        if self._available:
+            item_id = self._available.popleft()
+
+            file_key = self._data[item_id][1]
+            file_obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=file_key)
+            file_content = file_obj["Body"].read().decode("utf-8")
+            data = (file_key, file_content)
+
+            return DataPoolItem(item_id, data)
+        else:
+            raise Exception("Recyclable iterable datapool was emptied!")
+
+    async def checkin(self, item_id):
+        if self._available is None:
+            logger.error(
+                f"{repr(self)}: checkin called for {item_id} before the datapool "
+                "was initialized!  Maybe a stale runner is hanging around"
+            )
+            return
+        self._available.append(item_id)
