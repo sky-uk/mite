8fa1ed95c586e8e2fa62e5903e34515c6ab33cb6
Jenkins-CI <mite@noreply.github.com>
2025-04-09 11:51:17 +0100

    move docs

--- docs_old/conf.py
+++ docs_old/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'mite'
-copyright = '2019, Sky UK - Identity performance engineers'
-author = 'Sky UK - Identity performance engineers'
+project = "mite"
+copyright = "2019, Sky UK - Identity performance engineers"
+author = "Sky UK - Identity performance engineers"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,12 +30,12 @@ author = 'Sky UK - Identity performance engineers'
 extensions = ["sphinx.ext.graphviz", "sphinx.ext.intersphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -43,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 html_theme_options = {
     "github_user": "sky-uk",
@@ -55,7 +55,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
