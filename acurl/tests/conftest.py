3712c49a0aa621f761c363b7e7f0c0ac89ec3684
Jenkins-CI <mite@noreply.github.com>
2025-04-09 13:03:12 +0100

    test

--- acurl/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import acurl
-
-
-@pytest.fixture
-def acurl_session(event_loop):
-    w = acurl.CurlWrapper(event_loop)
-    yield w.session()
--- test/mocks/mock_sender.py
+++
@@ -1,6 +0,0 @@
-class SenderMock:
-    def __init__(self):
-        self.messages = []
-
-    def send(self, msg):
-        self.messages.append(msg)
