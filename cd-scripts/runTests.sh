05bffdc334ad8ab78ad2b2887fff6c851a0bd2da
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2025-08-22 11:47:01 +0100

    testing

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -19,16 +19,19 @@ if [ $PRE_COMMIT_STATUS -ne 0 ]; then
     git diff
 fi
 
+echo "##### Memory usage after tests #####"
+echo "##### Memory usage after tests #####"
 echo "##### Run acurl/tests/test_httpbin.py only (py311 via tox) #####"
 echo "##### Memory usage before tests #####"
 free -m || vm_stat
 ps
-TOXENV=py311 tox -- acurl/tests/test_httpbin.py -v; TOX_EXIT_CODE=$?
-[ "$TOX_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit 1
+TOXENV=py311 tox -- -v acurl/tests/test_httpbin.py; TOX_EXIT_CODE=$?
 
 echo "##### Memory usage after tests #####"
 free -m || vm_stat
 ps
+
+[ "$TOX_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit 1
 
 if isOnMaster ; then
     echo "##### Job running on MASTER. Proceeding with the Tag and Release script. ######"
