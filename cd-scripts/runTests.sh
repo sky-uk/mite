12add33d817d5077765be4e9535d8c71b3f7a378
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2025-08-22 11:21:28 +0100

    testing ci

--- cd-scripts/runTests.sh
+++ cd-scripts/runTests.sh
@@ -19,16 +19,17 @@ if [ $PRE_COMMIT_STATUS -ne 0 ]; then
     git diff
 fi
 
-echo "##### Run tests with tox #####"
+echo "##### Memory usage after tests #####"
+echo "##### Run tests with tox (py311 only) #####"
 echo "##### Memory usage before tests #####"
 free -m || vm_stat
-ps aux --sort=-%mem | head -20
-tox; TOX_EXIT_CODE=$?
+ps
+TOXENV=py311 tox; TOX_EXIT_CODE=$?
 [ "$TOX_EXIT_CODE" -eq 0 -a "$PRE_COMMIT_STATUS" -eq 0 ] || exit 1
 
 echo "##### Memory usage after tests #####"
 free -m || vm_stat
-ps aux --sort=-%mem | head -20
+ps
 
 if isOnMaster ; then
     echo "##### Job running on MASTER. Proceeding with the Tag and Release script. ######"
