77b3d4ffd25b4142c326c3fbdf8eb7fa4220bb54
Manasa Vemulapati <manasa.vemulapati@sky.uk>
2025-10-23 13:15:29 +0100

    undo change

--- mite_amqp/__init__.py
+++ mite_amqp/__init__.py
@@ -15,11 +15,7 @@ class AMQPError(MiteError):
 
 class _AMQPWrapper:
     def __init__(self):
-        try:
-            self._loop = asyncio.get_running_loop()
-        except RuntimeError:
-            self._loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self._loop)
+        self._loop = asyncio.get_event_loop()
 
     def install(self, context):
         context.amqp = self
