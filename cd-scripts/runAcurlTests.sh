e88ad23937f57ead1c77d57dfe3a6b3a6568dcda
dan69 <37105497+DavAnnunz@users.noreply.github.com>
2025-11-19 10:06:03 +0000

    fix

--- cd-scripts/runAcurlTests.sh
+++ cd-scripts/runAcurlTests.sh
@@ -4,7 +4,6 @@
 echo "##### Run tests with hatch #####"
 # Sort of an hack we are using hatch to run the unit tests here. 
 # For this reason we have to move back to the parent directory first.
-cd ..
 hatch run test.py3.11:acurl-test ; TESTS_EXIT_CODE=$?
 [ "$TESTS_EXIT_CODE" -eq 0 ] || exit 1
 echo "##### Tests passed #####"
