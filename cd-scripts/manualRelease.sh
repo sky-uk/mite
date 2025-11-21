b78f6f9bb56da4418ab19b5285e79f7a92b90796
Jenkins-CI <mite@noreply.github.com>
2025-11-21 15:50:11 +0000

    don't release

--- cd-scripts/manualRelease.sh
+++ cd-scripts/manualRelease.sh
@@ -11,21 +11,21 @@ if isOnMaster; then
     # If not passed, cdRelease.py will use the  GH PR to check which kind of increment it is supposed to be as VERSION_TYPE will be an empty string
     tagBuild ${VERSION_INCREMENT_TYPE}
 
-    echo "##### Look for Acurl changes #####"
-    checkAcurl
+    # echo "##### Look for Acurl changes #####"
+    # checkAcurl
 
-    echo "##### Build Linux-Wheels #####"
-    buildLinuxWheels
+    # echo "##### Build Linux-Wheels #####"
+    # buildLinuxWheels
 
-    if [ "$VERSION_INCREMENT" = false ] && [ "$ACURL_CHANGED" = false ]; then
-        echo "Mite version not incremented and Acurl not changed, nothing to upload"
-    else
-        echo "##### Init .pypirc #####"
-        initPypirc
+    # if [ "$VERSION_INCREMENT" = false ] && [ "$ACURL_CHANGED" = false ]; then
+    #     echo "Mite version not incremented and Acurl not changed, nothing to upload"
+    # else
+    #     echo "##### Init .pypirc #####"
+    #     initPypirc
 
-        echo "##### Upload package #####"
-        python3 -m twine upload wheelhouse/* 
-    fi
+    #     echo "##### Upload package #####"
+    #     python3 -m twine upload wheelhouse/* 
+    # fi
 
 else
 	echo "Not going to build the mite image because the job is not running on the master branch"
