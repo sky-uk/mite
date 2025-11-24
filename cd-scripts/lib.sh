e7e4a7ca65a72c83e4ef18563812c7233ca83592
Jenkins-CI <mite@noreply.github.com>
2025-11-24 09:55:30 +0000

    revert and fix

--- cd-scripts/lib.sh
+++ cd-scripts/lib.sh
@@ -11,12 +11,12 @@ function isOnMaster() {
 }
 
 function tagBuild() {   
-    VERSION_INCREMENT_TYPE="--${1}"
+    VERSION_INCREMENT_TYPE="${1}"
     git config user.email "mite@noreply.github.com"
     git config user.name "Jenkins-CI"
     pip3 install docopt GitPython packaging requests
     echo "Tagging the build with a ${VERSION_INCREMENT_TYPE} version increment"
-    python3 cd-scripts/cdTagRelease.py ${VERSION_INCREMENT_TYPE}
+    python3 cd-scripts/cdTagRelease.py --${VERSION_INCREMENT_TYPE}
     if (( $? == 1)); then
         VERSION_INCREMENT=false
     fi
--- cd-scripts/manualRelease.sh
+++ cd-scripts/manualRelease.sh
@@ -2,31 +2,28 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
-echo "VERSION_INCREMENT_TYPE is set to: $VERSION_INCREMENT_TYPE "
+if isOnMaster; then
+    echo "##### Job running on MASTER. Proceeding with the Tag and Release script. ######"
+    echo "##### Starting tag process and check for version increment #####"
+    # We are passing over the VERSION_INCREMENT_TYPE argument to tagBuild which will set the type of increment for the new version
+    tagBuild $VERSION_INCREMENT_TYPE
 
-# if isOnMaster; then
-echo "##### Job running on MASTER. Proceeding with the Tag and Release script. ######"
-echo "##### Starting tag process and check for version increment #####"
-# We are passing over the VERSION_TYPE argument to tagBuild which will set the type of increment for the new version
-# If not passed, cdRelease.py will use the  GH PR to check which kind of increment it is supposed to be as VERSION_TYPE will be an empty string
-tagBuild ${VERSION_INCREMENT_TYPE}
+    echo "##### Look for Acurl changes #####"
+    checkAcurl
 
-    # echo "##### Look for Acurl changes #####"
-    # checkAcurl
+    echo "##### Build Linux-Wheels #####"
+    buildLinuxWheels
 
-    # echo "##### Build Linux-Wheels #####"
-    # buildLinuxWheels
+    if [ "$VERSION_INCREMENT" = false ] && [ "$ACURL_CHANGED" = false ]; then
+        echo "Mite version not incremented and Acurl not changed, nothing to upload"
+    else
+        echo "##### Init .pypirc #####"
+        initPypirc
 
-    # if [ "$VERSION_INCREMENT" = false ] && [ "$ACURL_CHANGED" = false ]; then
-    #     echo "Mite version not incremented and Acurl not changed, nothing to upload"
-    # else
-    #     echo "##### Init .pypirc #####"
-    #     initPypirc
+        echo "##### Upload package #####"
+        python3 -m twine upload wheelhouse/* 
+    fi
 
-    #     echo "##### Upload package #####"
-    #     python3 -m twine upload wheelhouse/* 
-    # fi
-
-# else
-# 	echo "Not going to build the mite image because the job is not running on the master branch"
-# fi
+else
+	echo "Not going to build the mite image because the job is not running on the master branch"
+fi
