c5543d69e8446eb8b49751f6070ac57bf8ae6749
Davide Annunziata <davide.annunziata93@gmail.com>
2025-11-24 10:08:45 +0000

    Fix manualRelease Job + cleanup (#313)
    What is the change?
    Fix the release job
    
    Does this change require a version increment:
    It doesn't in theory but a MINOR release needs to be run because of the previous changes not been released yet.

--- cd-scripts/manualRelease.sh
+++ cd-scripts/manualRelease.sh
@@ -2,14 +2,11 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
-VERSION_INCREMENT_TYPE=${1}
-
 if isOnMaster; then
     echo "##### Job running on MASTER. Proceeding with the Tag and Release script. ######"
     echo "##### Starting tag process and check for version increment #####"
-    # We are passing over the VERSION_TYPE argument to tagBuild which will set the type of increment for the new version
-    # If not passed, cdRelease.py will use the  GH PR to check which kind of increment it is supposed to be as VERSION_TYPE will be an empty string
-    tagBuild ${VERSION_INCREMENT_TYPE}
+    # We are passing over the VERSION_INCREMENT_TYPE argument to tagBuild which will set the type of increment for the new version
+    tagBuild $VERSION_INCREMENT_TYPE
 
     echo "##### Look for Acurl changes #####"
     checkAcurl
